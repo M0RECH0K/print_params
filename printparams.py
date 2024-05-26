794cca15755525ff1b765d7a8434336e565646b0
Dmitry Kharlamov <168891004+M0RECH0K@users.noreply.github.com>
2024-05-26 15:40:44 +0300

    Update printparams.py

--- printparams.py
+++ printparams.py
@@ -1,45 +1,65 @@
-def print_params(a=1, b='строка', c=True):
-    print(a, b, c)
-    b = 25
-    c = [1, 2, 3]
-    print(a, b, c)
+# def print_params(a=1, b='строка', c=True):
+#     print(a, b, c)
+#     b = 25
+#     c = [1, 2, 3]
+#     print(a, b, c)
+#
+#
+# print_params()
+#
+#
+# def print_params(a, b, c, d):
+#     print(a, b, c, d)
+#
+#
+# print_params(1, 2, 3, 4)
 
 
-print_params()
+# def print_params():
+#     print()
+#
+#
+# print_params()
+#
+#
+# def print_params(c='[1,2,3]'):
+#     print(c)
+#
+#
+# print_params()
 
 
-def print_params(a=1, b=2):
-    print(a, b)
+# def print_params(b=25):
+#     print(b)
+#
+#
+# print_params()
 
 
-print_params()
+# def print_params(*args):
+#     print(args)
+#
+#
+# values_list = ['a', 1, True]
+# print_params(*values_list)
+#
+#
+# def print_params2(**kwargs):
+#     print(kwargs)
+#
+#
+# values_dict = {'a': 1, '1': 'b', 'True': [1, 2, 3]}
+# print_params2(**values_dict)
+#
+#
+# def print_params3():
+#     print(print_params3())
 
 
-def print_params():
-    print()
+# values_list_2 = ['a', 2]
+# print_params3(*values_list_2, 42)
 
 
-print_params()
-
-
-def print_params(*args):
-    print(args)
-
-
-values_list = ['a', 1, True]
-print_params(*values_list)
-
-
-def print_params2(**kwargs):
-    print(kwargs)
-
-
-values_dict = {'a': 1, '1': 'b', 'True': [1, 2, 3]}
-print_params2(**values_dict)
-
-
-def print_params3():
-    print(print_params3())
 
 
 # values_list_2 = ['a', 2]
